43ba092a95c84e31362c9718bcf0e16aff8c0e5f
bedardn93 <bedardn93@gmail.com>
2016-04-22 16:43:31 -0400

    Made changes to mouse look script
    'character model' no longer tilts when moving camera.  should be all
    good now

--- COSC-471-Project/Assets/mouse_controller.cs
+++ COSC-471-Project/Assets/mouse_controller.cs
@@ -2,29 +2,39 @@
 using System.Collections;
 
 public class mouse_controller : MonoBehaviour {
-	public float mouseSensitivity = 100.0f;
-	public float clampAngle = 80.0f;
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 };
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 15.0f;
+    public float sensitivityY = 15.0f;
+    public float minX = -360.0f;
+    public float maxX = 360.0f;
+    public float minY = -60.0f;
+    public float maxY = 60.0f;
 
 	private float rotY = 0.0f;
-	private float rotX = 0.0f;
 	// Use this for initialization
 	void Start () {
-		Vector3 rot = transform.localRotation.eulerAngles;
-		rotY = rot.y;
-		rotX = rot.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float mouseX = Input.GetAxis ("Mouse X");
-		float mouseY = Input.GetAxis ("Mouse Y");
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            float rotX = transform.localEulerAngles.y + mouseX + sensitivityX;
 
-		rotY += mouseX * mouseSensitivity * Time.deltaTime;
-		rotX += mouseY * mouseSensitivity * Time.deltaTime;
-
-		rotX = Mathf.Clamp (rotX, -clampAngle, clampAngle);
-
-		Quaternion localRotation = Quaternion.Euler (rotX, rotY, 0.0f);
-		transform.rotation = localRotation;
+            rotY += mouseY + sensitivityY;
+            rotY = Mathf.Clamp(rotY, minY, maxY);
+            transform.localEulerAngles = new Vector3(-rotY, rotX, 0);
+        }else if(axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, mouseX * sensitivityX, 0);
+        }else
+        {
+            rotY += mouseY * sensitivityY;
+            rotY = Mathf.Clamp(rotY, minY, maxY);
+            transform.localEulerAngles = new Vector3(-rotY, transform.localEulerAngles.y, 0);
+        }
 	}
 }
