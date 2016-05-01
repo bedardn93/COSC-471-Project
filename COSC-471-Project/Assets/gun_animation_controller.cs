d53a341564e105c35b538fb3009543adefbcd24d
bedardn93 <bedardn93@gmail.com>
2016-05-01 19:43:01 -0400

    Update to 2nd level and initial assault rifle
    Updated 2nd level so it has a galaxy skybox and moon texture applied to
    terrain.  Also began initial assault rifle programming.  It can shoot
    imaginary bullets and reload said imaginary bullets (w/ animation).

---
+++ COSC-471-Project/Assets/gun_animation_controller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gun_animation_controller : MonoBehaviour {
+	Animator anim;
+	int reloadHash = Animator.StringToHash("Reload");
+	float nextReload;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		nextReload = Time.fixedTime;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R) && Time.fixedTime >= nextReload) {
+			Debug.Log ("Reloading");
+			anim.SetBool (reloadHash, true);
+			nextReload = Time.fixedTime + 2.0f;
+		}
+	}
+}
---
+++ COSC-471-Project/Assets/gun_controller.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gun_controller : MonoBehaviour {
+	Canvas canvas;
+	GUIText text;
+	public int maxBullets = 30;
+	public int bullets = 30;
+	float nextShotTime;
+
+	// Use this for initialization
+	void Start () {
+		nextShotTime = Time.fixedTime + 0.1f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.Mouse0) && Time.fixedTime >= nextShotTime && bullets > 0) {
+			bullets--;
+			nextShotTime = Time.fixedTime + 0.1f;
+		}
+		if (Input.GetKey (KeyCode.R) && bullets < 30) {
+			bullets = maxBullets;
+		}
+	}
+}
