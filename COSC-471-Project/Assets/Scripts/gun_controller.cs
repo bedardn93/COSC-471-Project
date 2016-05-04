8efa4a3a0f27148416f525d88b5c8030c56280e1
bedardn93 <bedardn93@gmail.com>
2016-05-04 03:19:11 -0400

    Added bullet holes for shots fired and changed look of lasers

--- COSC-471-Project/Assets/Scripts/gun_controller.cs
+++ COSC-471-Project/Assets/Scripts/gun_controller.cs
@@ -11,6 +11,7 @@ public class gun_controller : MonoBehaviour {
 	float nextShot;
 	public Transform bulletSpawn;
 	public Transform projectile;
+	public Transform bulletHole;
 	public float bulletSpeed = 50.0f;
 	public float nextShotTime = 0.15f;
 	float nextReload;
@@ -25,9 +26,11 @@ public class gun_controller : MonoBehaviour {
 		nextReload = Time.fixedTime;
 		bullets = maxBullets;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+		Debug.DrawRay (transform.position, forward, Color.green);
 		if (Input.GetKey (KeyCode.Mouse0) && Time.fixedTime >= nextShot && bullets > 0 && Time.fixedTime >= nextReload) {
 			shoot ();
 		}
@@ -57,10 +60,13 @@ public class gun_controller : MonoBehaviour {
 		Rigidbody clone = obj.GetComponent<Rigidbody> ();
 		//Play shooting sound
 		audio.Play ();
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		RaycastHit hit;
-		if (Physics.Raycast (ray, out hit, 1000.0f))
+		Ray ray = new Ray(transform.position, transform.forward);
+		RaycastHit hit;		 
+		if (Physics.Raycast (ray, out hit, 1000.0f)) {
 			Debug.DrawLine (ray.origin, hit.point);
+			Quaternion bulletHoleRotation = Quaternion.FromToRotation (Vector3.up, hit.normal);
+			Instantiate (bulletHole, hit.point, bulletHoleRotation);
+		}
 		//Apply force up y-axis (green axis)
 		clone.AddForce (clone.transform.up * bulletSpeed);
 		bullets--;
