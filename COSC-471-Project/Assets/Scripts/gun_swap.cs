c980757633ed945a5fa431b9a72b4a26dde7fba8
bedardn93 <bedardn93@gmail.com>
2016-05-08 17:04:49 -0400

    gun swapping

---
+++ COSC-471-Project/Assets/Scripts/gun_swap.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gun_swap : MonoBehaviour
+{
+	public int currentWeapon;
+	public GameObject[] weapons;
+
+	void Start(){
+		
+	}
+    
+	void Update (){
+		if (Input.GetKey (KeyCode.Alpha1))
+			swap (0);
+		if (Input.GetKey (KeyCode.Alpha2))
+			swap (1);
+		if (Input.GetKey (KeyCode.Alpha3))
+			swap (2);
+	}
+
+	public void swap(int num) {
+        currentWeapon = num;
+		for (int i = 0; i < weapons.Length; i++) {
+			if (i == num)
+				weapons [i].gameObject.SetActive (true);
+			else
+				weapons [i].gameObject.SetActive (false);
+		}
+     }
+
+}
